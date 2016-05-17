51b1901b21690ecf4cca6d583f6faf414f7c1882
Rajesh Gopidi <rgopidi@cisco.com>
2016-05-16 20:55:43 -0700

    Adding skeleton classes for new functionality

---
+++ src/main/java/GetDealers.java
@@ -0,0 +1,27 @@
+/**
+ * Created by rgopidi on 5/16/16.
+ */
+public class GetDealers {
+
+    private String address = "2000 Walnut Ave, Apt T307, Fremont, CA, 94538";
+    private double latitude = 0;
+    private double lngitude = 0;
+    private String key = "AIzaSyB4jWfGSqJsoL5kE9Gt0O7TClIv65o-VmA";
+
+
+    public void getLatAndLong (String addr)
+    {
+
+
+
+    }
+
+    public static void main (String[] args)
+    {
+
+
+
+
+    }
+
+}
---
+++ src/main/java/HttpsConn.java
@@ -0,0 +1,5 @@
+/**
+ * Created by rgopidi on 5/16/16.
+ */
+public class HttpsConn {
+}
